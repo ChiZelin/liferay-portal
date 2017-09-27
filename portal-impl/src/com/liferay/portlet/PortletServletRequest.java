62795b15a4da3054c68842aadbe05a2be2b9f9a5
leon.chi <leon.chi@liferay.com>
2017-09-27 16:09:30 +0800

    LPS-X Wrap RequestDispatcher into PortletRequestDispatcher

--- portal-impl/src/com/liferay/portlet/PortletServletRequest.java
+++ portal-impl/src/com/liferay/portlet/PortletServletRequest.java
@@ -378,7 +378,15 @@ public class PortletServletRequest extends HttpServletRequestWrapper {
 
 	@Override
 	public RequestDispatcher getRequestDispatcher(String path) {
-		return _request.getRequestDispatcher(path);
+		RequestDispatcher requestDispatcher = _request.getRequestDispatcher(
+			path);
+
+		if (requestDispatcher != null) {
+			requestDispatcher = new PortletRequestDispatcherImpl(
+				requestDispatcher);
+		}
+
+		return requestDispatcher;
 	}
 
 	@Override
