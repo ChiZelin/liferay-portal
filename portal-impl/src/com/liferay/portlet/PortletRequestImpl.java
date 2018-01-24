2eb34b15e735c00c50af766bfa17c624177d8f71
leon.chi <leon.chi@liferay.com>
2018-01-24 14:29:33 +0800

    LPS-X Add reservedAttr

--- portal-impl/src/com/liferay/portlet/PortletRequestImpl.java
+++ portal-impl/src/com/liferay/portlet/PortletRequestImpl.java
@@ -1175,6 +1175,7 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 		_reservedAttrs.add(WebKeys.PORTLET_ID);
 		_reservedAttrs.add(WebKeys.THEME_DISPLAY);
 		_reservedAttrs.add(WebKeys.WINDOW_STATE);
+		_reservedAttrs.add(WebKeys.LAYOUT);
 	}
 
 	private Map<String, Object> _actionScopedRequestAttributesPool;
