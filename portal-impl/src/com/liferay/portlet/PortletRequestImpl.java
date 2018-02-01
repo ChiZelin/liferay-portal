103ba5922535472c42deb873bc565ade3d589377
leon.chi <leon.chi@liferay.com>
2018-02-01 08:12:54 +0800

    LPS-X Always return false

--- portal-impl/src/com/liferay/portlet/PortletRequestImpl.java
+++ portal-impl/src/com/liferay/portlet/PortletRequestImpl.java
@@ -1064,7 +1064,7 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 			return false;
 		}
 
-		return true;
+		return false;
 	}
 
 	private void _mergePublicRenderParameters(
