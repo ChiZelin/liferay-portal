0cc45924fe7a30115cc6e67f18797410dba6763e
leon.chi <leon.chi@liferay.com>
2018-02-02 10:35:57 +0800

    Revert "LPS-X Always return false"
    This reverts commit e97eab06d881b4c8d4d1658f2db87ce589297a66.

--- portal-impl/src/com/liferay/portlet/PortletRequestImpl.java
+++ portal-impl/src/com/liferay/portlet/PortletRequestImpl.java
@@ -1061,7 +1061,7 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 			return false;
 		}
 
-		return false;
+		return true;
 	}
 
 	private void _mergePublicRenderParameters(
