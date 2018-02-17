95705dcedfbd9b1f84fd4333c61ab2d8fc72a221
Neil Griffin <neil.griffin.scm@gmail.com>
2018-02-17 16:05:12 -0500

    LPS-73282 Implement Portlet 3.0 (implemented most of the Java API)

--- portal-kernel/src/com/liferay/portal/kernel/portlet/PortletURLFactoryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/PortletURLFactoryUtil.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
 
+import javax.portlet.MimeResponse;
 import javax.portlet.PortletRequest;
 
 import javax.servlet.http.HttpServletRequest;
@@ -36,6 +37,14 @@ public class PortletURLFactoryUtil {
 	}
 
 	public static LiferayPortletURL create(
+		HttpServletRequest request, Portlet portlet, Layout layout,
+		String lifecycle, MimeResponse.Copy copy) {
+
+		return getPortletURLFactory().create(
+			request, portlet, layout, lifecycle, copy);
+	}
+
+	public static LiferayPortletURL create(
 		HttpServletRequest request, Portlet portlet, String lifecycle) {
 
 		return getPortletURLFactory().create(request, portlet, lifecycle);
@@ -47,6 +56,14 @@ public class PortletURLFactoryUtil {
 
 		return getPortletURLFactory().create(
 			request, portletId, layout, lifecycle);
+	}
+
+	public static LiferayPortletURL create(
+		HttpServletRequest request, String portletId, Layout layout,
+		String lifecycle, MimeResponse.Copy copy) {
+
+		return getPortletURLFactory().create(
+			request, portletId, layout, lifecycle, copy);
 	}
 
 	public static LiferayPortletURL create(
@@ -80,6 +97,14 @@ public class PortletURLFactoryUtil {
 	}
 
 	public static LiferayPortletURL create(
+		PortletRequest portletRequest, Portlet portlet, long plid,
+		String lifecycle, MimeResponse.Copy copy) {
+
+		return getPortletURLFactory().create(
+			portletRequest, portlet, plid, lifecycle, copy);
+	}
+
+	public static LiferayPortletURL create(
 		PortletRequest portletRequest, String portletId, Layout layout,
 		String lifecycle) {
 
@@ -93,6 +118,14 @@ public class PortletURLFactoryUtil {
 
 		return getPortletURLFactory().create(
 			portletRequest, portletId, plid, lifecycle);
+	}
+
+	public static LiferayPortletURL create(
+		PortletRequest portletRequest, String portletId, long plid,
+		String lifecycle, MimeResponse.Copy copy) {
+
+		return getPortletURLFactory().create(
+			portletRequest, portletId, plid, lifecycle, copy);
 	}
 
 	public static LiferayPortletURL create(
