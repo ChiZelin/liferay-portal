47c405da83dcbd302fc8861937d8dc0fc09e94eb
Leon Chi <leon.chi@liferay.com>
2018-08-09 15:51:19 +0800

    LPS-X Modify the PropsInvocationHandler

--- modules/apps/portal-cache/portal-cache-ehcache-impl/src/test/java/com/liferay/portal/cache/ehcache/internal/configurator/PropsInvocationHandler.java
+++ modules/apps/portal-cache/portal-cache-ehcache-impl/src/test/java/com/liferay/portal/cache/ehcache/internal/configurator/PropsInvocationHandler.java
@@ -27,12 +27,30 @@ import java.util.Properties;
  */
 public class PropsInvocationHandler implements InvocationHandler {
 
+	public static final String[] ARRAY_0 = {};
+
+	public static final String[] ARRAY_1 = {"value"};
+
+	public static final String[] ARRAY_2 = {"value1", "value2"};
+
 	public static final String[]
 		EHCACHE_BOOTSTRAP_CACHE_LOADER_PROPERTIES_DEFAULT_VALUE =
 			{"value5", "value6"};
 
+	public static final Properties
+		EHCACHE_BOOTSTRAP_CACHE_LOADER_PROPERTIES_VALUE = new Properties();
+
+	public static final Properties
+		EHCACHE_BOOTSTRAP_CACHE_LOADER_PROPERTIES_VALUE1 = new Properties();
+
 	public static final String[] EHCACHE_REPLICATOR_PROPERTIES_DEFAULT_VALUE =
 		{"value7", "value8"};
+
+	public static final Properties EHCACHE_REPLICATOR_PROPERTIES_VALUE =
+		new Properties();
+
+	public static final Properties EHCACHE_REPLICATOR_PROPERTIES_VALUE1 =
+		new Properties();
 
 	public static final String EHCACHE_RMI_PEER_LISTENER_FACTORY_CLASS_VALUE =
 		"ehcache.rmi.peer.listener.factory.class.value";
@@ -48,8 +66,34 @@ public class PropsInvocationHandler implements InvocationHandler {
 		EHCACHE_RMI_PEER_PROVIDER_FACTORY_PROPERTIES_VALUE =
 			{"value3", "value4"};
 
+	public static final String PORTAL_PROPERTY_KEY0 = "portal.property.Key0";
+
+	public static final String PORTAL_PROPERTY_KEY1 = "portal.property.Key1";
+
+	public static final String PORTAL_PROPERTY_KEY2 = "portal.property.Key2";
+
+	static {
+		EHCACHE_BOOTSTRAP_CACHE_LOADER_PROPERTIES_VALUE1.setProperty(
+			"name1", "value1");
+		EHCACHE_BOOTSTRAP_CACHE_LOADER_PROPERTIES_VALUE1.setProperty(
+			"name2", "value2");
+		EHCACHE_BOOTSTRAP_CACHE_LOADER_PROPERTIES_VALUE1.setProperty(
+			"name3", "value3");
+
+		EHCACHE_REPLICATOR_PROPERTIES_VALUE1.setProperty("name1", "value1");
+		EHCACHE_REPLICATOR_PROPERTIES_VALUE1.setProperty("name2", "value2");
+		EHCACHE_REPLICATOR_PROPERTIES_VALUE1.setProperty("name3", "value3");
+	}
+
 	public PropsInvocationHandler(boolean clusterEnabled) {
 		_clusterEnabled = clusterEnabled;
+	}
+
+	public PropsInvocationHandler(
+		boolean clusterEnabled, boolean bootstrapLoaderEnabled) {
+
+		_clusterEnabled = clusterEnabled;
+		_bootstrapLoaderEnabled = bootstrapLoaderEnabled;
 	}
 
 	@Override
@@ -69,6 +113,15 @@ public class PropsInvocationHandler implements InvocationHandler {
 
 			if (PropsKeys.CLUSTER_LINK_ENABLED.equals(key)) {
 				if (_clusterEnabled) {
+					return "true";
+				}
+				else {
+					return "false";
+				}
+			}
+
+			if (PropsKeys.EHCACHE_BOOTSTRAP_CACHE_LOADER_ENABLED.equals(key)) {
+				if (_bootstrapLoaderEnabled) {
 					return "true";
 				}
 				else {
@@ -101,6 +154,18 @@ public class PropsInvocationHandler implements InvocationHandler {
 			if (PropsKeys.EHCACHE_REPLICATOR_PROPERTIES_DEFAULT.equals(key)) {
 				return EHCACHE_REPLICATOR_PROPERTIES_DEFAULT_VALUE;
 			}
+
+			if (PORTAL_PROPERTY_KEY0.equals(key)) {
+				return ARRAY_0;
+			}
+
+			if (PORTAL_PROPERTY_KEY1.equals(key)) {
+				return ARRAY_1;
+			}
+
+			if (PORTAL_PROPERTY_KEY2.equals(key)) {
+				return ARRAY_2;
+			}
 		}
 
 		if (methodName.equals("getProperties")) {
@@ -110,13 +175,21 @@ public class PropsInvocationHandler implements InvocationHandler {
 					PropsKeys.EHCACHE_REPLICATOR_PROPERTIES +
 						StringPool.PERIOD)) {
 
-				return new Properties();
+				return EHCACHE_REPLICATOR_PROPERTIES_VALUE;
+			}
+
+			if (key.equals(
+					PropsKeys.EHCACHE_BOOTSTRAP_CACHE_LOADER_PROPERTIES +
+						StringPool.PERIOD)) {
+
+				return EHCACHE_BOOTSTRAP_CACHE_LOADER_PROPERTIES_VALUE;
 			}
 		}
 
 		return null;
 	}
 
+	private boolean _bootstrapLoaderEnabled;
 	private final boolean _clusterEnabled;
 
 }
