9c1e4e96212a70fac48e6d65a68866e2f22e9b72
Leon Chi <leon.chi@liferay.com>
2018-12-12 16:03:45 +0800

    LPS-88366 Apply InvokerFilter.getURI(String)

--- portal-impl/test/unit/com/liferay/portal/kernel/servlet/filters/invoker/InvokerFilterTest.java
+++ portal-impl/test/unit/com/liferay/portal/kernel/servlet/filters/invoker/InvokerFilterTest.java
@@ -68,16 +68,7 @@ public class InvokerFilterTest {
 			mockHttpServletRequest);
 
 		Assert.assertEquals(
-			"/c/portal/login",
-			invokerFilter.getURI(mockHttpServletRequest, originalURI));
-
-		mockHttpServletRequest = new MockHttpServletRequest(
-			HttpMethods.GET,
-			"/c///portal/%2e/../login;jsessionid=ae01b0f2af.worker1");
-
-		Assert.assertEquals(
-			"/c/portal/login",
-			invokerFilter.getURI(mockHttpServletRequest, originalURI));
+			"/c/portal/login", invokerFilter.getURI(originalURI));
 	}
 
 	@Test
@@ -93,8 +84,7 @@ public class InvokerFilterTest {
 			mockHttpServletRequest);
 
 		Assert.assertEquals(
-			"/c/portal/login",
-			invokerFilter.getURI(mockHttpServletRequest, originalURI));
+			"/c/portal/login", invokerFilter.getURI(originalURI));
 	}
 
 	@Test
