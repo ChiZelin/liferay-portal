52000a62816257477c40616376e373a018e736b0
Leon Chi <leon.chi@liferay.com>
2019-01-09 15:34:49 +0800

    LPS-88392 Move related test class into UserGroupMembershipPolicyFactoryImplTest as inner class

--- portal-impl/test/integration/com/liferay/portal/security/membershippolicy/UserGroupMembershipPolicyFactoryImplTest.java
+++ portal-impl/test/integration/com/liferay/portal/security/membershippolicy/UserGroupMembershipPolicyFactoryImplTest.java
@@ -14,17 +14,20 @@
 
 package com.liferay.portal.security.membershippolicy;
 
+import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.security.membershippolicy.UserGroupMembershipPolicy;
 import com.liferay.portal.kernel.security.membershippolicy.UserGroupMembershipPolicyFactory;
 import com.liferay.portal.kernel.security.membershippolicy.UserGroupMembershipPolicyFactoryUtil;
 import com.liferay.portal.kernel.security.membershippolicy.UserGroupMembershipPolicyUtil;
-import com.liferay.portal.security.membershippolicy.bundle.usergroupmembershippolicyfactoryimpl.TestUserGroupMembershipPolicy;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
 
+import java.io.Serializable;
+
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.AfterClass;
@@ -157,5 +160,66 @@ public class UserGroupMembershipPolicyFactoryImplTest {
 	private static final AtomicBoolean _atomicBoolean = new AtomicBoolean();
 	private static ServiceRegistration<UserGroupMembershipPolicy>
 		_serviceRegistration;
+
+	private static class TestUserGroupMembershipPolicy
+		implements UserGroupMembershipPolicy {
+
+		@Override
+		public void checkMembership(
+			long[] userIds, long[] addUserGroupIds, long[] removeUserGroupIds) {
+
+			_atomicBoolean.set(Boolean.TRUE);
+		}
+
+		@Override
+		public boolean isMembershipAllowed(long userId, long userGroupId) {
+			if (userId == 1) {
+				return true;
+			}
+
+			return false;
+		}
+
+		@Override
+		public boolean isMembershipRequired(long userId, long userGroupId) {
+			if (userId == 1) {
+				return true;
+			}
+
+			return false;
+		}
+
+		@Override
+		public void propagateMembership(
+			long[] userIds, long[] addUserGroupIds, long[] removeUserGroupIds) {
+
+			_atomicBoolean.set(Boolean.TRUE);
+		}
+
+		public void setAtomicBoolean(AtomicBoolean atomicBoolean) {
+			_atomicBoolean = atomicBoolean;
+		}
+
+		@Override
+		public void verifyPolicy() {
+			_atomicBoolean.set(Boolean.TRUE);
+		}
+
+		@Override
+		public void verifyPolicy(UserGroup userGroup) {
+			_atomicBoolean.set(Boolean.TRUE);
+		}
+
+		@Override
+		public void verifyPolicy(
+			UserGroup userGroup, UserGroup oldUserGroup,
+			Map<String, Serializable> oldExpandoAttributes) {
+
+			_atomicBoolean.set(Boolean.TRUE);
+		}
+
+		private AtomicBoolean _atomicBoolean;
+
+	}
 
 }
