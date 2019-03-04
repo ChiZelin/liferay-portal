5176a352651286dae76b781260937e09bf5a3f9f
Leon Chi <leon.chi@liferay.com>
2019-03-04 09:46:45 +0800

    LPS-X Run with Arquillian

--- modules/test/portal-kernel-test/src/testIntegration/java/com/liferay/portal/kernel/transaction/test/TransactionInvokerUtilTest.java
+++ modules/test/portal-kernel-test/src/testIntegration/java/com/liferay/portal/kernel/transaction/test/TransactionInvokerUtilTest.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.kernel.transaction.test;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.counter.kernel.service.CounterLocalServiceUtil;
 import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
@@ -30,10 +31,12 @@ import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Shuyang Zhou
  */
+@RunWith(Arquillian.class)
 public class TransactionInvokerUtilTest {
 
 	@ClassRule
