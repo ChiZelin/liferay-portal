d166723244e1ef89ba3001995fceb678a214a371
Leon Chi <leon.chi@liferay.com>
2019-03-06 15:32:05 +0800

    LPS-88392 Move related test class into SocialActivityInterpreterLocalServiceImplTest as inner class

--- portal-impl/test/integration/com/liferay/portlet/social/service/impl/SocialActivityInterpreterLocalServiceImplTest.java
+++ portal-impl/test/integration/com/liferay/portlet/social/service/impl/SocialActivityInterpreterLocalServiceImplTest.java
@@ -14,12 +14,16 @@
 
 package com.liferay.portlet.social.service.impl;
 
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portlet.social.service.impl.bundle.socialactivityinterpreterlocalserviceimpl.TestSocialActivityInterpreter;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
+import com.liferay.social.kernel.model.SocialActivity;
+import com.liferay.social.kernel.model.SocialActivityFeedEntry;
 import com.liferay.social.kernel.model.SocialActivityInterpreter;
+import com.liferay.social.kernel.model.SocialActivitySet;
 import com.liferay.social.kernel.service.SocialActivityInterpreterLocalServiceUtil;
 
 import java.util.Arrays;
@@ -117,5 +121,48 @@ public class SocialActivityInterpreterLocalServiceImplTest {
 
 	private static ServiceRegistration<SocialActivityInterpreter>
 		_serviceRegistration;
+
+	private static class TestSocialActivityInterpreter
+		implements SocialActivityInterpreter {
+
+		public static final String SELECTOR = "SELECTOR";
+
+		@Override
+		public String[] getClassNames() {
+			return new String[] {TestSocialActivityInterpreter.class.getName()};
+		}
+
+		@Override
+		public String getSelector() {
+			return SELECTOR;
+		}
+
+		@Override
+		public boolean hasPermission(
+			PermissionChecker permissionChecker, SocialActivity activity,
+			String actionId, ServiceContext serviceContext) {
+
+			return false;
+		}
+
+		@Override
+		public SocialActivityFeedEntry interpret(
+			SocialActivity activity, ServiceContext serviceContext) {
+
+			return null;
+		}
+
+		@Override
+		public SocialActivityFeedEntry interpret(
+			SocialActivitySet activitySet, ServiceContext serviceContext) {
+
+			return null;
+		}
+
+		@Override
+		public void updateActivitySet(long activityId) {
+		}
+
+	}
 
 }
