e9d653ba50eaead7a645035ec62fc6c9caf0943f
Leon Chi <leon.chi@liferay.com>
2019-03-06 15:13:00 +0800

    LPS-88392 Change PollerProcessorUtilTest to unit test

--- portal-impl/test/integration/com/liferay/portal/poller/PollerProcessorUtilTest.java
+++ portal-impl/test/integration/com/liferay/portal/poller/PollerProcessorUtilTest.java
@@ -17,7 +17,7 @@ package com.liferay.portal.poller;
 import com.liferay.portal.kernel.poller.PollerProcessor;
 import com.liferay.portal.kernel.poller.PollerRequest;
 import com.liferay.portal.kernel.poller.PollerResponse;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.BasicRegistryImpl;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
@@ -27,8 +27,6 @@ import java.util.HashMap;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -36,13 +34,10 @@ import org.junit.Test;
  */
 public class PollerProcessorUtilTest {
 
-	@ClassRule
-	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
-
 	@BeforeClass
 	public static void setUpClass() {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration = registry.registerService(
