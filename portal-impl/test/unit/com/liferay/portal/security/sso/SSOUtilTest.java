c2102b76be0ab07437afa474190767a8152c47dc
Leon Chi <leon.chi@liferay.com>
2019-03-06 16:22:10 +0800

    LPs-88392 Change SSOUtilTest to unit test

--- portal-impl/test/unit/com/liferay/portal/security/sso/SSOUtilTest.java
+++ portal-impl/test/unit/com/liferay/portal/security/sso/SSOUtilTest.java
@@ -16,36 +16,33 @@ package com.liferay.portal.security.sso;
 
 import com.liferay.portal.kernel.security.sso.SSO;
 import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.PrefsPropsUtil;
+import com.liferay.registry.BasicRegistryImpl;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.portlet.PortletPreferences;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
-
-import javax.portlet.PortletPreferences;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Peter Fellwock
  */
 public class SSOUtilTest {
 
-	@ClassRule
-	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
-
 	@BeforeClass
 	public static void setUpClass() {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration = registry.registerService(
