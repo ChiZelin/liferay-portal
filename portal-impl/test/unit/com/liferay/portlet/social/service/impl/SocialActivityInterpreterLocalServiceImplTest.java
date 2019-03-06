cc6c9ebe71e3cf2fa47aba4a42f7631daf1d66cd
Leon Chi <leon.chi@liferay.com>
2019-03-06 15:38:09 +0800

    LPS-88392 Change SocialActivityInterpreterLocalServiceImplTest to unit test

--- portal-impl/test/unit/com/liferay/portlet/social/service/impl/SocialActivityInterpreterLocalServiceImplTest.java
+++ portal-impl/test/unit/com/liferay/portlet/social/service/impl/SocialActivityInterpreterLocalServiceImplTest.java
@@ -16,7 +16,7 @@ package com.liferay.portlet.social.service.impl;
 
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.BasicRegistryImpl;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
@@ -34,8 +34,6 @@ import java.util.Map;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -43,13 +41,10 @@ import org.junit.Test;
  */
 public class SocialActivityInterpreterLocalServiceImplTest {
 
-	@ClassRule
-	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
-
 	@BeforeClass
 	public static void setUpClass() {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration = registry.registerService(
