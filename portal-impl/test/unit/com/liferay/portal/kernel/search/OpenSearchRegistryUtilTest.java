0e532867f769e387e91f91827d00ab7d870b1c78
Leon Chi <leon.chi@liferay.com>
2019-03-07 14:36:39 +0800

    LPS-94013 Change OpenSearchRegistryUtilTest to unit test

--- portal-impl/test/unit/com/liferay/portal/kernel/search/OpenSearchRegistryUtilTest.java
+++ portal-impl/test/unit/com/liferay/portal/kernel/search/OpenSearchRegistryUtilTest.java
@@ -14,7 +14,7 @@
 
 package com.liferay.portal.kernel.search;
 
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.BasicRegistryImpl;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
@@ -26,8 +26,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -35,13 +33,10 @@ import org.junit.Test;
  */
 public class OpenSearchRegistryUtilTest {
 
-	@ClassRule
-	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
-
 	@BeforeClass
 	public static void setUpClass() {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration = registry.registerService(
