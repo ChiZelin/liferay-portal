c7026e827767a361b64add299837d2f02462af01
Leon Chi <leon.chi@liferay.com>
2019-03-07 14:49:49 +0800

    LPS-88392 Move TemplateResourceLoaderUtilTest from integration to unit

--- portal-impl/test/unit/com/liferay/portal/kernel/template/TemplateResourceLoaderUtilTest.java
+++ portal-impl/test/unit/com/liferay/portal/kernel/template/TemplateResourceLoaderUtilTest.java
@@ -14,8 +14,6 @@
 
 package com.liferay.portal.kernel.template;
 
-import com.liferay.portal.kernel.template.bundle.templateresourceloaderutil.TestTemplateResource;
-import com.liferay.portal.kernel.template.bundle.templateresourceloaderutil.TestTemplateResourceLoader;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
