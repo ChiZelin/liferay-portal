8157eaf1037dafd7e4097cb20871e273ba22f160
Leon Chi <leon.chi@liferay.com>
2019-03-18 09:19:34 +0800

    LPS-88392 Change RepositoryClassDefinitionCatalogImplTest to unit test

--- portal-impl/test/unit/com/liferay/portal/repository/registry/RepositoryClassDefinitionCatalogImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/repository/registry/RepositoryClassDefinitionCatalogImplTest.java
@@ -21,7 +21,7 @@ import com.liferay.portal.kernel.repository.registry.CapabilityRegistry;
 import com.liferay.portal.kernel.repository.registry.RepositoryDefiner;
 import com.liferay.portal.kernel.repository.registry.RepositoryEventRegistry;
 import com.liferay.portal.kernel.repository.registry.RepositoryFactoryRegistry;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.BasicRegistryImpl;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
@@ -33,8 +33,6 @@ import java.util.Locale;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -42,13 +40,10 @@ import org.junit.Test;
  */
 public class RepositoryClassDefinitionCatalogImplTest {
 
-	@ClassRule
-	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
-
 	@BeforeClass
 	public static void setUpClass() {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration1 = registry.registerService(
