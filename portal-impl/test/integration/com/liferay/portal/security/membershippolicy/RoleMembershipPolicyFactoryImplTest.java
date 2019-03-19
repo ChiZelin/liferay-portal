f11cf0a2b87a169f28a1e28a1f1ceba5e7d2c587
Leon Chi <leon.chi@liferay.com>
2019-03-19 08:49:58 +0800

    LPS-88392 Move related test class into RoleMembershipPolicyFactoryImplTest as inner class

--- portal-impl/test/integration/com/liferay/portal/security/membershippolicy/RoleMembershipPolicyFactoryImplTest.java
+++ portal-impl/test/integration/com/liferay/portal/security/membershippolicy/RoleMembershipPolicyFactoryImplTest.java
@@ -14,12 +14,12 @@
 
 package com.liferay.portal.security.membershippolicy;
 
+import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.security.membershippolicy.RoleMembershipPolicy;
 import com.liferay.portal.kernel.security.membershippolicy.RoleMembershipPolicyFactory;
 import com.liferay.portal.kernel.security.membershippolicy.RoleMembershipPolicyFactoryUtil;
 import com.liferay.portal.kernel.security.membershippolicy.RoleMembershipPolicyUtil;
 import com.liferay.portal.model.impl.RoleImpl;
-import com.liferay.portal.security.membershippolicy.bundle.rolemembershippolicyfactoryimpl.TestRoleMembershipPolicy;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.test.AtomicState;
 import com.liferay.registry.Registry;
@@ -29,6 +29,8 @@ import com.liferay.registry.ServiceRegistration;
 import java.io.Serializable;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -160,5 +162,66 @@ public class RoleMembershipPolicyFactoryImplTest {
 	private static AtomicState _atomicState;
 	private static ServiceRegistration<RoleMembershipPolicy>
 		_serviceRegistration;
+
+	private static class TestRoleMembershipPolicy
+		implements RoleMembershipPolicy {
+
+		@Override
+		public void checkRoles(
+			long[] userIds, long[] addRoleIds, long[] removeRoleIds) {
+
+			_atomicBoolean.set(Boolean.TRUE);
+		}
+
+		@Override
+		public boolean isRoleAllowed(long userId, long roleId) {
+			if (userId == 1) {
+				return true;
+			}
+
+			return false;
+		}
+
+		@Override
+		public boolean isRoleRequired(long userId, long roleId) {
+			if (userId == 1) {
+				return true;
+			}
+
+			return false;
+		}
+
+		@Override
+		public void propagateRoles(
+			long[] userIds, long[] addRoleIds, long[] removeRoleIds) {
+
+			_atomicBoolean.set(Boolean.TRUE);
+		}
+
+		@Override
+		public void verifyPolicy() {
+			_atomicBoolean.set(Boolean.TRUE);
+		}
+
+		@Override
+		public void verifyPolicy(Role role) {
+			_atomicBoolean.set(Boolean.TRUE);
+		}
+
+		@Override
+		public void verifyPolicy(
+			Role role, Role oldRole,
+			Map<String, Serializable> oldExpandoAttributes) {
+
+			_atomicBoolean.set(Boolean.TRUE);
+		}
+
+		protected void setAtomicBoolean(AtomicBoolean atomicBoolean) {
+			_atomicBoolean = atomicBoolean;
+		}
+
+		private AtomicBoolean _atomicBoolean;
+
+	}
 
 }
