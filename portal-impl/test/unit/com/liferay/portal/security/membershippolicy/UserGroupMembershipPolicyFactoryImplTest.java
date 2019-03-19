f52188da6da0734492cf780a5823fd225039127c
Leon Chi <leon.chi@liferay.com>
2019-03-19 10:24:22 +0800

    LPs-88392 Change UserGroupMembershipPolicyFactoryImplTest to unit test

--- portal-impl/test/unit/com/liferay/portal/security/membershippolicy/UserGroupMembershipPolicyFactoryImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/security/membershippolicy/UserGroupMembershipPolicyFactoryImplTest.java
@@ -19,7 +19,7 @@ import com.liferay.portal.kernel.security.membershippolicy.UserGroupMembershipPo
 import com.liferay.portal.kernel.security.membershippolicy.UserGroupMembershipPolicyFactory;
 import com.liferay.portal.kernel.security.membershippolicy.UserGroupMembershipPolicyFactoryUtil;
 import com.liferay.portal.kernel.security.membershippolicy.UserGroupMembershipPolicyUtil;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.BasicRegistryImpl;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
@@ -32,8 +32,6 @@ import java.util.Map;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -41,13 +39,10 @@ import org.junit.Test;
  */
 public class UserGroupMembershipPolicyFactoryImplTest {
 
-	@ClassRule
-	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
-
 	@BeforeClass
 	public static void setUpClass() {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration = registry.registerService(
