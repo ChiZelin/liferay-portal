7e76e066e29169a2b62383ebe55e9777228d5f4c
Leon Chi <leon.chi@liferay.com>
2019-03-19 14:04:08 +0800

    LPS-88392 Change MVCActionCommandTest to unit test

--- portal-impl/test/unit/com/liferay/portal/kernel/portlet/bridges/mvc/MVCActionCommandTest.java
+++ portal-impl/test/unit/com/liferay/portal/kernel/portlet/bridges/mvc/MVCActionCommandTest.java
@@ -20,7 +20,7 @@ import com.liferay.portal.kernel.portlet.LiferayPortletConfig;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.test.rule.Inject;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.BasicRegistryImpl;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
@@ -51,8 +51,6 @@ import javax.xml.namespace.QName;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -65,13 +63,10 @@ import org.springframework.mock.web.portlet.MockPortletConfig;
  */
 public class MVCActionCommandTest {
 
-	@ClassRule
-	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
-
 	@BeforeClass
 	public static void setUpClass() {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration1 = registry.registerService(
