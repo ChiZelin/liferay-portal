835d772ea69646f8a1b0a4e99c605b526514f2d3
Leon Chi <leon.chi@liferay.com>
2019-03-19 13:40:04 +0800

    LPS-88392 Change JMXWhiteboardTest to unit test

--- portal-impl/test/unit/com/liferay/portal/jmx/JMXWhiteboardTest.java
+++ portal-impl/test/unit/com/liferay/portal/jmx/JMXWhiteboardTest.java
@@ -15,7 +15,7 @@
 package com.liferay.portal.jmx;
 
 import com.liferay.portal.test.rule.Inject;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.BasicRegistryImpl;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
@@ -34,8 +34,6 @@ import javax.management.StandardMBean;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -43,13 +41,10 @@ import org.junit.Test;
  */
 public class JMXWhiteboardTest {
 
-	@ClassRule
-	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
-
 	@BeforeClass
 	public static void setUpClass() throws NotCompliantMBeanException {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration1 = registry.registerService(
