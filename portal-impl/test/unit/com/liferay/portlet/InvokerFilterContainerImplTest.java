f9713e1df17a094176a89c32fc84b1b29ca133c7
Leon Chi <leon.chi@liferay.com>
2019-03-19 16:44:11 +0800

    LPS-88392 Change InvokerFilterContainerImplTest to unit test

--- portal-impl/test/unit/com/liferay/portlet/InvokerFilterContainerImplTest.java
+++ portal-impl/test/unit/com/liferay/portlet/InvokerFilterContainerImplTest.java
@@ -21,10 +21,10 @@ import com.liferay.portal.kernel.portlet.PortletContextFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.model.impl.PortletAppImpl;
 import com.liferay.portal.model.impl.PortletImpl;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.callback.MainServletTestCallback;
 import com.liferay.portlet.internal.InvokerFilterContainerImpl;
 import com.liferay.portlet.internal.PortletContextFactoryImpl;
+import com.liferay.registry.BasicRegistryImpl;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
@@ -55,8 +55,6 @@ import javax.servlet.ServletContext;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -65,13 +63,10 @@ import org.junit.Test;
  */
 public class InvokerFilterContainerImplTest {
 
-	@ClassRule
-	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
-
 	@BeforeClass
 	public static void setUpClass() {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration1 = registry.registerService(
