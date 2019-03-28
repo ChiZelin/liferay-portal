27449a7a3bf4c54bd5f91de15fa42fad3d06d53a
Leon Chi <leon.chi@liferay.com>
2019-03-28 14:04:54 +0800

    LPS-88392 Change corresponding *Test to unit test

--- portal-impl/test/unit/com/liferay/portal/kernel/lar/xstream/XStreamConverterRegistryUtilTest.java
+++ portal-impl/test/unit/com/liferay/portal/kernel/lar/xstream/XStreamConverterRegistryUtilTest.java
@@ -20,7 +20,7 @@ import com.liferay.exportimport.kernel.xstream.XStreamHierarchicalStreamReader;
 import com.liferay.exportimport.kernel.xstream.XStreamHierarchicalStreamWriter;
 import com.liferay.exportimport.kernel.xstream.XStreamMarshallingContext;
 import com.liferay.exportimport.kernel.xstream.XStreamUnmarshallingContext;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.BasicRegistryImpl;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
@@ -31,8 +31,6 @@ import java.util.Set;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -40,13 +38,10 @@ import org.junit.Test;
  */
 public class XStreamConverterRegistryUtilTest {
 
-	@ClassRule
-	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
-
 	@BeforeClass
 	public static void setUpClass() {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration = registry.registerService(
