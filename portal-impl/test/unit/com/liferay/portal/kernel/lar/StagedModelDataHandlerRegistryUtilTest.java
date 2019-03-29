1a631a557fc4c9d8cfc4bee67b253306c31125b4
Dante Wang <dante.wang@liferay.com>
2019-03-29 15:28:21 +0800

    LPS-88392 Simplify the test using proxy

--- portal-impl/test/unit/com/liferay/portal/kernel/lar/StagedModelDataHandlerRegistryUtilTest.java
+++ portal-impl/test/unit/com/liferay/portal/kernel/lar/StagedModelDataHandlerRegistryUtilTest.java
@@ -14,18 +14,15 @@
 
 package com.liferay.portal.kernel.lar;
 
-import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.StagedModelDataHandler;
 import com.liferay.exportimport.kernel.lar.StagedModelDataHandlerRegistryUtil;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.registry.BasicRegistryImpl;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
 
 import java.util.List;
-import java.util.Map;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -43,8 +40,20 @@ public class StagedModelDataHandlerRegistryUtilTest {
 
 		Registry registry = RegistryUtil.getRegistry();
 
+		_stagedModelDataHandler =
+			(StagedModelDataHandler<?>)ProxyUtil.newProxyInstance(
+				StagedModelDataHandlerRegistryUtilTest.class.getClassLoader(),
+				new Class<?>[] {StagedModelDataHandler.class},
+				(proxy, method, args) -> {
+					if ("getClassNames".equals(method.getName())) {
+						return CLASS_NAMES;
+					}
+
+					return null;
+				});
+
 		_serviceRegistration = registry.registerService(
-			StagedModelDataHandler.class, new TestStagedModelDataHandler());
+			StagedModelDataHandler.class, _stagedModelDataHandler);
 	}
 
 	@AfterClass
@@ -56,12 +65,9 @@ public class StagedModelDataHandlerRegistryUtilTest {
 	public void testGetStagedModelDataHandler() {
 		StagedModelDataHandler<?> stagedModelDataHandler =
 			StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(
-				TestStagedModelDataHandler.CLASS_NAMES[0]);
+				CLASS_NAMES[0]);
 
-		Class<?> clazz = stagedModelDataHandler.getClass();
-
-		Assert.assertEquals(
-			TestStagedModelDataHandler.class.getName(), clazz.getName());
+		Assert.assertSame(_stagedModelDataHandler, stagedModelDataHandler);
 	}
 
 	@Test
@@ -69,129 +75,19 @@ public class StagedModelDataHandlerRegistryUtilTest {
 		List<StagedModelDataHandler<?>> stagedModelDataHandlers =
 			StagedModelDataHandlerRegistryUtil.getStagedModelDataHandlers();
 
-		String testClassName = TestStagedModelDataHandler.class.getName();
-
-		Assert.assertTrue(
-			testClassName + " not found in " + stagedModelDataHandlers,
-			stagedModelDataHandlers.removeIf(
-				stagedModelDataHandler -> {
-					Class<?> clazz = stagedModelDataHandler.getClass();
-
-					return testClassName.equals(clazz.getName());
-				}));
+		Assert.assertEquals(
+			stagedModelDataHandlers.toString(), 1,
+			stagedModelDataHandlers.size());
+		Assert.assertSame(
+			_stagedModelDataHandler, stagedModelDataHandlers.get(0));
 	}
+
+	public static final String[] CLASS_NAMES = {
+		StagedModelDataHandlerRegistryUtilTest.class.getName()
+	};
 
 	private static ServiceRegistration<StagedModelDataHandler>
 		_serviceRegistration;
-
-	private static class TestStagedModelDataHandler
-		implements StagedModelDataHandler<User> {
-
-		public static final String[] CLASS_NAMES = {
-			TestStagedModelDataHandler.class.getName()
-		};
-
-		@Override
-		public void deleteStagedModel(
-			String uuid, long groupId, String className, String extraData) {
-		}
-
-		@Override
-		public void deleteStagedModel(User user) {
-		}
-
-		@Override
-		public void exportStagedModel(
-			PortletDataContext portletDataContext, User user) {
-		}
-
-		@Override
-		public User fetchMissingReference(String uuid, long groupId) {
-			return null;
-		}
-
-		@Override
-		public User fetchStagedModelByUuidAndGroupId(
-			String uuid, long groupId) {
-
-			return null;
-		}
-
-		@Override
-		public List<User> fetchStagedModelsByUuidAndCompanyId(
-			String uuid, long companyId) {
-
-			return null;
-		}
-
-		@Override
-		public String[] getClassNames() {
-			return CLASS_NAMES;
-		}
-
-		@Override
-		public String getDisplayName(User user) {
-			return null;
-		}
-
-		@Override
-		public int[] getExportableStatuses() {
-			return null;
-		}
-
-		@Override
-		public Map<String, String> getReferenceAttributes(
-			PortletDataContext portletDataContext, User user) {
-
-			return null;
-		}
-
-		/**
-		 * @deprecated As of Wilberforce (7.0.x)
-		 */
-		@Deprecated
-		@Override
-		public void importCompanyStagedModel(
-			PortletDataContext portletDataContext, Element element) {
-		}
-
-		/**
-		 * @deprecated As of Wilberforce (7.0.x)
-		 */
-		@Deprecated
-		@Override
-		public void importCompanyStagedModel(
-			PortletDataContext portletDataContext, String uuid, long classPK) {
-		}
-
-		@Override
-		public void importMissingReference(
-			PortletDataContext portletDataContext, Element referenceElement) {
-		}
-
-		@Override
-		public void importMissingReference(
-			PortletDataContext portletDataContext, String uuid, long groupId,
-			long classPK) {
-		}
-
-		@Override
-		public void importStagedModel(
-			PortletDataContext portletDataContext, User user) {
-		}
-
-		@Override
-		public void restoreStagedModel(
-			PortletDataContext portletDataContext, User user) {
-		}
-
-		@Override
-		public boolean validateReference(
-			PortletDataContext portletDataContext, Element referenceElement) {
-
-			return false;
-		}
-
-	}
+	private static StagedModelDataHandler<?> _stagedModelDataHandler;
 
 }
