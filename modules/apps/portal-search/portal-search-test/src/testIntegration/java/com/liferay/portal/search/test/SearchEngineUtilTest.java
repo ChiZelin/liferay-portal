8ce8ce639a31f6d8536176498b66fe3011284d68
Leon Chi <leon.chi@liferay.com>
2019-04-03 15:32:45 +0800

    LPS-94013 Move SearchEngineUtilTest to module portal-search-test

--- modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/test/SearchEngineUtilTest.java
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/test/SearchEngineUtilTest.java
@@ -12,8 +12,10 @@
  * details.
  */
 
-package com.liferay.portal.kernel.search;
+package com.liferay.portal.search.test;
 
+import com.liferay.portal.kernel.search.SearchEngine;
+import com.liferay.portal.kernel.search.SearchEngineConfigurator;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
