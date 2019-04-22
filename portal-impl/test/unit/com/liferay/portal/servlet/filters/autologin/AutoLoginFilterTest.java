131c4e3d4062f5ad6c40dd6114e578521a250ce5
Leon Chi <leon.chi@liferay.com>
2019-04-22 09:43:23 +0800

    LPS-94205 Make AutoLoginFilter unit test

--- portal-impl/test/unit/com/liferay/portal/servlet/filters/autologin/AutoLoginFilterTest.java
+++ portal-impl/test/unit/com/liferay/portal/servlet/filters/autologin/AutoLoginFilterTest.java
@@ -15,11 +15,14 @@
 package com.liferay.portal.servlet.filters.autologin;
 
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.security.auto.login.AutoLogin;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.ProxyFactory;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.test.rule.SyntheticBundleClassTestRule;
-import com.liferay.portal.util.test.AtomicState;
+import com.liferay.portal.util.PortalImpl;
+import com.liferay.registry.BasicRegistryImpl;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceRegistration;
 
 import java.io.IOException;
 
@@ -27,45 +30,43 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 
 /**
- * @author Philip Jones
+ * @author Leon Chi
  */
 public class AutoLoginFilterTest {
 
-	@ClassRule
-	@Rule
-	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(),
-			new SyntheticBundleClassTestRule("bundle.autologinfilter"));
-
 	@BeforeClass
 	public static void setUpClass() {
-		_atomicState = new AtomicState();
+		PortalUtil portalUtil = new PortalUtil();
+
+		portalUtil.setPortal(new PortalImpl());
+
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
+		Registry registry = RegistryUtil.getRegistry();
+
+		_serviceRegistration = registry.registerService(
+			AutoLogin.class, new TestAutoLogin());
 	}
 
 	@AfterClass
 	public static void tearDownClass() {
-		_atomicState.close();
+		_serviceRegistration.unregister();
 	}
 
 	@Test
 	public void testDoFilter() throws IOException, ServletException {
+		_called = false;
+
 		AutoLoginFilter autoLoginFilter = new AutoLoginFilter();
-
-		FilterChain filterChain = ProxyFactory.newDummyInstance(
-			FilterChain.class);
-
-		_atomicState.reset();
 
 		autoLoginFilter.doFilter(
 			new HttpServletRequestWrapper(
@@ -82,11 +83,33 @@ public class AutoLoginFilterTest {
 				}
 
 			},
-			null, filterChain);
+			null, ProxyFactory.newDummyInstance(FilterChain.class));
 
-		Assert.assertTrue(_atomicState.isSet());
+		Assert.assertTrue(_called);
 	}
 
-	private static AtomicState _atomicState;
+	private static boolean _called;
+	private static ServiceRegistration<AutoLogin> _serviceRegistration;
+
+	private static class TestAutoLogin implements AutoLogin {
+
+		@Override
+		public String[] handleException(
+			HttpServletRequest request, HttpServletResponse response,
+			Exception e) {
+
+			return null;
+		}
+
+		@Override
+		public String[] login(
+			HttpServletRequest request, HttpServletResponse response) {
+
+			_called = true;
+
+			return null;
+		}
+
+	}
 
 }
