861bff6c86ecb843783ceb2fcf3a0fc0c9d3d7e0
Leon Chi <leon.chi@liferay.com>
2019-07-16 12:43:01 +0800

    LPS-97532 Replace String.format in dynamic-data-mapping-form-web

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormAdminDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormAdminDisplayContext.java
@@ -1270,10 +1270,9 @@ public class DDMFormAdminDisplayContext {
 		}
 		catch (JSONException jsone) {
 			_log.error(
-				String.format(
-					"Unable to deserialize JSON localized property \"%s\" " +
-						"from request",
-					propertyName),
+				StringBundler.concat(
+					"Unable to deserialize JSON localized property \"",
+					propertyName, "\" from request", propertyName),
 				jsone);
 		}
 
