e020d35ee0f19e7c268e577a2b49d114f9462a87
Leon Chi <leon.chi@liferay.com>
2019-12-26 09:33:29 +0800

    LPS-106881 Replace com.liferay.portal.kernel.model.Subscription with com.liferay.subscription.model.Subscription and replace com.liferay.portal.kernel.service.SubscriptionLocalServiceUtil with com.liferay.subscription.service.SubscriptionLocalServiceUtil

--- modules/apps/subscription/subscription-web/src/main/java/com/liferay/subscription/web/internal/util/UnsubscribeHooks.java
+++ modules/apps/subscription/subscription-web/src/main/java/com/liferay/subscription/web/internal/util/UnsubscribeHooks.java
@@ -96,9 +96,7 @@ public class UnsubscribeHooks {
 		}
 	}
 
-	public void createUnsubscriptionTicket(
-		com.liferay.portal.kernel.model.Subscription subscription) {
-
+	public void createUnsubscriptionTicket(Subscription subscription) {
 		if (_subscriptionSender.isBulk()) {
 			return;
 		}
@@ -139,9 +137,7 @@ public class UnsubscribeHooks {
 		mailMessage.setBody(processedBody);
 	}
 
-	private Ticket _getTicket(
-		com.liferay.portal.kernel.model.Subscription subscription) {
-
+	private Ticket _getTicket(Subscription subscription) {
 		Calendar calendar = Calendar.getInstance();
 
 		calendar.add(
