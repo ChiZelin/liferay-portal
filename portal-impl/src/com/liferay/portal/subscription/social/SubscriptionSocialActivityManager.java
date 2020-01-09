e8c7494f784cb50fa7ca515168adbef67490d2c1
Leon Chi <leon.chi@liferay.com>
2020-01-09 18:14:48 +0800

    LPS-106881 Copy SubscriptionSocialActivityManager back and deprecate; remove configuration from social-spring.xml

---
+++ portal-impl/src/com/liferay/portal/subscription/social/SubscriptionSocialActivityManager.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.subscription.social;
+
+import com.liferay.portal.kernel.bean.BeanReference;
+import com.liferay.portal.kernel.model.Subscription;
+import com.liferay.portal.kernel.social.BaseSocialActivityManager;
+import com.liferay.social.kernel.service.SocialActivityLocalService;
+
+/**
+ * @author Adolfo Pérez
+ * @deprecated As of Athanasius (7.3.x), replaced by {@link
+ *			 com.liferay.subscription.internal.social.SubscriptionSocialActivityManager}
+ */
+@Deprecated
+public class SubscriptionSocialActivityManager
+	extends BaseSocialActivityManager<Subscription> {
+
+	@Override
+	protected String getClassName(Subscription subscription) {
+		return subscription.getClassName();
+	}
+
+	@Override
+	protected long getPrimaryKey(Subscription subscription) {
+		return subscription.getClassPK();
+	}
+
+	@Override
+	protected SocialActivityLocalService getSocialActivityLocalService() {
+		return socialActivityLocalService;
+	}
+
+	@BeanReference(type = SocialActivityLocalService.class)
+	protected SocialActivityLocalService socialActivityLocalService;
+
+}
