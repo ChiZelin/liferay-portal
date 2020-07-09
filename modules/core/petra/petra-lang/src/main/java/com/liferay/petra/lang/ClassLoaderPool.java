175c9b6f0187e907f58048594df63c49c2765bc7
Leon Chi <leon.chi@liferay.com>
2020-07-09 13:57:07 +0800

    LPS-116325 Merge registerFallback and unregisterFallback logic from ClusterClassLoaderPool to ClassLoaderPool

--- modules/core/petra/petra-lang/src/main/java/com/liferay/petra/lang/ClassLoaderPool.java
+++ modules/core/petra/petra-lang/src/main/java/com/liferay/petra/lang/ClassLoaderPool.java
@@ -15,7 +15,11 @@
 package com.liferay.petra.lang;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * Maps servlet context names to/from the servlet context's class loader.
@@ -40,6 +44,26 @@ public class ClassLoaderPool {
 
 		if ((contextName != null) && !contextName.equals("null")) {
 			classLoader = _classLoaders.get(contextName);
+
+			if (classLoader == null) {
+				int pos = contextName.indexOf("_");
+
+				if (pos > 0) {
+					String symbolicName = contextName.substring(0, pos);
+
+					ConcurrentNavigableMap<Version, ClassLoader> classLoaders =
+						_fallbackClassLoaders.get(symbolicName);
+
+					if (classLoaders != null) {
+						Map.Entry<Version, ClassLoader> entry =
+							classLoaders.lastEntry();
+
+						if (entry != null) {
+							classLoader = entry.getValue();
+						}
+					}
+				}
+			}
 		}
 
 		if (classLoader == null) {
@@ -80,6 +104,22 @@ public class ClassLoaderPool {
 	public static void register(String contextName, ClassLoader classLoader) {
 		_classLoaders.put(contextName, classLoader);
 		_contextNames.put(classLoader, contextName);
+
+		if (contextName.lastIndexOf("_") > 0) {
+			String[] str = contextName.split("_");
+
+			_fallbackClassLoaders.compute(
+				str[0],
+				(key, classLoaders) -> {
+					if (classLoaders == null) {
+						classLoaders = new ConcurrentSkipListMap<>();
+					}
+
+					classLoaders.put(new Version(str[1]), classLoader);
+
+					return classLoaders;
+				});
+		}
 	}
 
 	public static void unregister(ClassLoader classLoader) {
@@ -87,6 +127,12 @@ public class ClassLoaderPool {
 
 		if (contextName != null) {
 			_classLoaders.remove(contextName);
+
+			if (contextName.lastIndexOf("_") > 0) {
+				String[] str = contextName.split("_");
+
+				_unregisterFallback(str[0], new Version(str[1]));
+			}
 		}
 	}
 
@@ -96,15 +142,252 @@ public class ClassLoaderPool {
 		if (classLoader != null) {
 			_contextNames.remove(classLoader);
 		}
+
+		if (contextName.lastIndexOf("_") > 0) {
+			String[] str = contextName.split("_");
+
+			_unregisterFallback(str[0], new Version(str[1]));
+		}
+	}
+
+	private static void _unregisterFallback(
+		String symbolicName, Version version) {
+
+		_fallbackClassLoaders.computeIfPresent(
+			symbolicName,
+			(key, classLoaders) -> {
+				classLoaders.remove(version);
+
+				if (classLoaders.isEmpty()) {
+					return null;
+				}
+
+				return classLoaders;
+			});
 	}
 
 	private static final Map<String, ClassLoader> _classLoaders =
 		new ConcurrentHashMap<>();
 	private static final Map<ClassLoader, String> _contextNames =
 		new ConcurrentHashMap<>();
+	private static final Map
+		<String, ConcurrentNavigableMap<Version, ClassLoader>>
+			_fallbackClassLoaders = new ConcurrentHashMap<>();
 
 	static {
+		register("SystemClassLoader", ClassLoader.getSystemClassLoader());
 		register("GlobalClassLoader", ClassLoaderPool.class.getClassLoader());
+	}
+
+	private static class Version implements Comparable<Version> {
+
+		public Version(String version) {
+			int major = 0;
+			int minor = 0;
+			int micro = 0;
+			String qualifier = "";
+
+			try {
+				StringTokenizer stringTokenizer = new StringTokenizer(
+					version, _SEPARATOR, true);
+
+				major = _parseInt(stringTokenizer.nextToken(), version);
+
+				if (stringTokenizer.hasMoreTokens()) {
+					stringTokenizer.nextToken();
+
+					minor = _parseInt(stringTokenizer.nextToken(), version);
+
+					if (stringTokenizer.hasMoreTokens()) {
+						stringTokenizer.nextToken();
+
+						micro = _parseInt(stringTokenizer.nextToken(), version);
+
+						if (stringTokenizer.hasMoreTokens()) {
+							stringTokenizer.nextToken();
+
+							qualifier = stringTokenizer.nextToken("");
+						}
+					}
+				}
+			}
+			catch (NoSuchElementException noSuchElementException) {
+				IllegalArgumentException illegalArgumentException =
+					new IllegalArgumentException(
+						"invalid version \"" + version + "\": invalid format");
+
+				illegalArgumentException.initCause(noSuchElementException);
+
+				throw illegalArgumentException;
+			}
+
+			_major = major;
+			_minor = minor;
+			_micro = micro;
+			_qualifier = qualifier;
+			_validate();
+		}
+
+		@Override
+		public int compareTo(Version other) {
+			if (other == this) {
+				return 0;
+			}
+
+			int result = _major - other._major;
+
+			if (result != 0) {
+				return result;
+			}
+
+			result = _minor - other._minor;
+
+			if (result != 0) {
+				return result;
+			}
+
+			result = _micro - other._micro;
+
+			if (result != 0) {
+				return result;
+			}
+
+			return _qualifier.compareTo(other._qualifier);
+		}
+
+		public boolean equals(Object object) {
+			if (object == this) {
+				return true;
+			}
+
+			if (!(object instanceof Version)) {
+				return false;
+			}
+
+			Version other = (Version)object;
+
+			if ((_major == other._major) && (_minor == other._minor) &&
+				(_micro == other._micro) &&
+				_qualifier.equals(other._qualifier)) {
+
+				return true;
+			}
+
+			return false;
+		}
+
+		public int hashCode() {
+			int h = _hash;
+
+			if (h != 0) {
+				return h;
+			}
+
+			h = 31 * 17;
+			h = (31 * h) + _major;
+			h = (31 * h) + _minor;
+			h = (31 * h) + _micro;
+			h = (31 * h) + _qualifier.hashCode();
+
+			return _hash = h;
+		}
+
+		public String toString() {
+			return _toString0();
+		}
+
+		private int _parseInt(String value, String version) {
+			try {
+				return Integer.parseInt(value);
+			}
+			catch (NumberFormatException numberFormatException) {
+				IllegalArgumentException illegalArgumentException =
+					new IllegalArgumentException(
+						"invalid version \"" + version + "\": non-numeric \"" +
+							value + "\"");
+
+				illegalArgumentException.initCause(numberFormatException);
+
+				throw illegalArgumentException;
+			}
+		}
+
+		private String _toString0() {
+			String s = _versionString;
+
+			if (s != null) {
+				return s;
+			}
+
+			int q = _qualifier.length();
+
+			StringBuffer result = new StringBuffer(20 + q);
+
+			result.append(_major);
+			result.append(_SEPARATOR);
+			result.append(_minor);
+			result.append(_SEPARATOR);
+			result.append(_micro);
+
+			if (q > 0) {
+				result.append(_SEPARATOR);
+				result.append(_qualifier);
+			}
+
+			return _versionString = result.toString();
+		}
+
+		private void _validate() {
+			if (_major < 0) {
+				throw new IllegalArgumentException(
+					"invalid version \"" + _toString0() +
+						"\": negative number \"" + _major + "\"");
+			}
+
+			if (_minor < 0) {
+				throw new IllegalArgumentException(
+					"invalid version \"" + _toString0() +
+						"\": negative number \"" + _minor + "\"");
+			}
+
+			if (_micro < 0) {
+				throw new IllegalArgumentException(
+					"invalid version \"" + _toString0() +
+						"\": negative number \"" + _micro + "\"");
+			}
+
+			for (char ch : _qualifier.toCharArray()) {
+				if (('A' <= ch) && (ch <= 'Z')) {
+					continue;
+				}
+
+				if (('a' <= ch) && (ch <= 'z')) {
+					continue;
+				}
+
+				if (('0' <= ch) && (ch <= '9')) {
+					continue;
+				}
+
+				if ((ch == '_') || (ch == '-')) {
+					continue;
+				}
+
+				throw new IllegalArgumentException(
+					"invalid version \"" + _toString0() +
+						"\": invalid qualifier \"" + _qualifier + "\"");
+			}
+		}
+
+		private static final String _SEPARATOR = ".";
+
+		private transient int _hash;
+		private final int _major;
+		private final int _micro;
+		private final int _minor;
+		private final String _qualifier;
+		private transient String _versionString;
+
 	}
 
 }
